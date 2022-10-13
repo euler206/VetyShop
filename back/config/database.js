0f9dc3e01f11b503d779446443b9d61ef5438684
Euler Basante <euler206@gmail.com>
2022-10-12 20:13:09 -0500

    listo create

--- back/config/database.js
+++ back/config/database.js
@@ -4,8 +4,10 @@ const connectDatabase = () => {
     mongoose.connect(process.env.DB_LOCAL_URI, {
         useNewUrlParser:true,
         useUnifiedTopology:true
-    }).then(con=>{
+    }).then(con => {
         console.log(`Base de datos conectada con el servidor: ${con.connection.host}`)
+    }).catch(con => {
+        console.log(`No se logro la conexion con la base de datos`) 
     })
 }
 
--- back/routes/products.js
+++ back/routes/products.js
@@ -1,8 +1,11 @@
 const express=require("express")
 const router=express.Router();
 
-const {getProducts} = require("../controllers/productsController") //Traemos la respuesta json desde el controlador
+const {getProducts, newProduct} = require("../controllers/productsController") //Traemos la respuesta json desde el controlador
 
-router.route('/productos').get(getProducts) //Establecemos desde qyue ruta queremos ver el getProductos
+router.route('/productos').get(getProducts) // Establecemos desde qyue ruta queremos ver el getProductos
+router.route('/producto/nuevo').post(newProduct); //establecimeos la ruta
+
+
 
 module.exports=router;
